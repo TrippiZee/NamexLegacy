4782c410b49e1d4ec6d1f1d7a339806d707b5dfe
Stephan van Rensburg <stephan.vanrensburg@za.didata.com>
2017-02-09 18:40:44 +0200

    quick change

--- public/index.php
+++ public/index.php
@@ -4,8 +4,8 @@ require($basePath."vendor/autoload.php");
 require($basePath."includes/bootstrapper.php");
 use Includes\Router;
 use Includes\Request;
-$router = new Router;
-require $basePath.'routes.php';
+//$router = new Router;
+//require $basePath.'routes.php';
 //$uri = trim(parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH),'/');
 
 //$router = Router::load($basePath.'routes.php');
