13f3712a2499f3576155a965823bd6740586ad76
TrippiZee <stephan@codekraft.co.za>
2017-02-15 14:48:43 +0200

    pass connection

--- includes/controllers/manifestController.php
+++ includes/controllers/manifestController.php
@@ -8,7 +8,8 @@ class ManifestController {
 
     public function allManifests(){
 
-        return view('manifest');
+        global $connection;
+        return view('manifest',['connection'=>$connection]);
     }
 
     public function filterManifests()
